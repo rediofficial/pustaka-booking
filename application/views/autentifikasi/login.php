f404defb04a4af2280e9d7bc6d7ab2dc46283ca4
rediofficial <Rediofficial27@gmail.com>
2022-10-13 04:36:22 -0500

    Sudah ada Daftar Akun

--- application/views/autentifikasi/login.php
+++ application/views/autentifikasi/login.php
@@ -15,7 +15,7 @@
                                 </div>
                                 <form class="user">
                                     <div class="form-group">
-                                        <input type="email" class="form-control formcontrol-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                        <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                                     </div>
                                     <div class="form-group">
                                         <input type="password" class="form-control
@@ -24,15 +24,15 @@ form-control-user" id="exampleInputPassword" placeholder="Password">
                                     <div class="form-group">
                                         <div class="custom-control custom-checkbox
 small">
-                                            <input type="checkbox" class="customcontrol-input" id="customCheck">
+                                            <input type="checkbox" class="custom-control-input" id="customCheck">
                                             <label class="custom-control-label" for="customCheck">Remember Me</label>
                                         </div>
                                     </div>
-                                    <a href="latihan" class="btn btn-primary btnuser btn-block">
+                                    <a href="latihan" class="btn btn-primary btn-user btn-block">
                                         Login
                                     </a>
                                     <hr>
-                                    <a href="index.html" class="btn btn-google btnuser btn-block">
+                                    <a href="index.html" class="btn btn-google btn-user btn-block">
                                         <i class="fab fa-google fa-fw"></i> Login with
                                         Google
                                     </a>
@@ -47,7 +47,7 @@ btn-user btn-block">
                                     <a class="small" href="forgotpassword.html">Forgot Password?</a>
                                 </div>
                                 <div class="text-center">
-                                    <a class="small" href="register.html">Create an
+                                    <a class="small" href="autentifikasi/registrasi">Create an
                                         Account!</a>
                                 </div>
                             </div>
